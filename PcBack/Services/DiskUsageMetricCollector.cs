d321987f3b75d311edc2b750a789069c05e9e37d
Дмитрий Лёвкин <jonikm892@gmail.com>
2025-06-06 18:25:09 +0300

    Добавленны сборщики метрик с диска и температуры процессора

---
+++ PcBack/Services/DiskUsageMetricCollector.cs
@@ -0,0 +1,31 @@
+﻿using PcBack.Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcBack.Services
+{
+    public class DiskUsageMetricCollector : IMetricCollector
+    {
+        private readonly PerformanceCounter diskCounter =
+            new PerformanceCounter("LogicalDisk", "% Disk Time", "_Total");
+
+        public IEnumerable<CollectedMetricValue> Collect()
+        {
+            var value = new CollectedMetricValue
+            {
+                MetricName = "DiskUsage (%)",
+                Value = Convert.ToDecimal(diskCounter.NextValue()),
+                RecordedAt = DateTime.Now
+            };
+
+            Task.Delay(500).Wait();
+            value.Value = Convert.ToDecimal(diskCounter.NextValue());
+
+            yield return value;
+        }
+    }
+}
---
+++ PcBack/Services/TemperatureMetricCollector.cs
@@ -0,0 +1,51 @@
+﻿using OpenHardwareMonitor.Hardware;
+using PcBack.Data.Models;
+using PcBack.Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcBack.Services
+{
+    public class TemperatureMetricCollector : IMetricCollector
+    {
+        private readonly OpenHardwareMonitor.Hardware.Computer _ohmComputer;
+
+        public TemperatureMetricCollector()
+        {
+            _ohmComputer = new OpenHardwareMonitor.Hardware.Computer
+            {
+                CPUEnabled = true,
+                GPUEnabled = true,
+                FanControllerEnabled = false,
+                RAMEnabled = false,
+                MainboardEnabled = false,
+                HDDEnabled = false
+            };
+            _ohmComputer.Open();
+        }
+
+        public IEnumerable<CollectedMetricValue> Collect()
+        {
+            foreach (var hardware in _ohmComputer.Hardware)
+            {
+                hardware.Update();
+
+                foreach (var sensor in hardware.Sensors)
+                {
+                    if (sensor.SensorType == SensorType.Temperature)
+                    {
+                        yield return new CollectedMetricValue
+                        {
+                            MetricName = $"{hardware.Name} Temperature",
+                            Value = (decimal)Math.Round(sensor.Value ?? 0, 2),
+                            RecordedAt = DateTime.Now
+                        };
+                    }
+                }
+            }
+        }
+    }
+}
